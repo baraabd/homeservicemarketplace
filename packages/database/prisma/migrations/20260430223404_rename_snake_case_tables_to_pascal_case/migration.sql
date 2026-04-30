36ac081ef93246603d540cb0392084ffe2a7b73d
Baraa Abdullatif <55985500+baraabd@users.noreply.github.com>
2026-05-01 00:46:15 +0200

    refactor(database): rename tables to pascal case

--- packages/database/prisma/migrations/20260430223404_rename_snake_case_tables_to_pascal_case/migration.sql
+++
@@ -1,6 +0,0 @@
-﻿ALTER TABLE "user_roles" RENAME TO "UserRole";
-ALTER TABLE "role_permissions" RENAME TO "RolePermission";
-ALTER TABLE "verification_tokens" RENAME TO "VerificationToken";
-ALTER TABLE "oauth_accounts" RENAME TO "OauthAccount";
-ALTER TABLE "mfa_backup_codes" RENAME TO "MfaBackupCode";
-ALTER TABLE "audit_events" RENAME TO "AuditEvent";
